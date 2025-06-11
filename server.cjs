00ff4f4d87d818df0a7643fa6810712e2b46ce8b
Nazarian <Nazarian_2000_year@mail.ru>
2025-06-11 23:49:56 +0300

    sprint_1, fix server.cjs

--- server.cjs
+++ server.cjs
@@ -5,9 +5,9 @@ const app = express();
 const PORT = 3000;
 app.use(express.static('./dist'));
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './dist/index.html'));
   res.status(200);
 });
 
-app.listen(PORT, () => `Server started on port ${PORT}`);
+app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
